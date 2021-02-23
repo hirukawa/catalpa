93a1b80b8cd8a9ae5f70f2d9a6375d76a2c7958b
hirukawa_ryo <hirukawa_ryo@609f12cb-285d-4f60-a4be-869a2bfbb8d5>
2021-02-23 03:44:57 +0000

    * catalpa 0.8.3 依存ライブラリ freemarker 2.3.30 -> 2.3.31 依存ライブラリ ikonli 11.5.0 -> 12.2.0 MarkdownDIrective に relativeUrlPrefix が指定されている場合、Freemarker の変数展開を適用するようにしました。 これはブログAddOnでトップページやカテゴリーページに記事冒頭が表示されるときに該当します。

--- src/main/java/net/osdn/catalpa/Catalpa.java
+++ src/main/java/net/osdn/catalpa/Catalpa.java
@@ -195,7 +195,7 @@ public class Catalpa {
 	public void process(Path outputPath, Map<String, Object> options, ProgressObserver observer) throws Exception {
 		this.observer = (observer != null) ? observer : ProgressObserver.EMPTY;
 		
-		freeMarker = new Configuration(Configuration.VERSION_2_3_30);
+		freeMarker = new Configuration(Configuration.VERSION_2_3_31);
 		freeMarker.setDefaultEncoding("UTF-8");
 		freeMarker.setURLEscapingCharset("UTF-8");
 		freeMarker.setSharedVariable("baseurl", new BaseurlMethod());
